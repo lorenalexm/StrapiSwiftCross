b54ceb538c07be616afa4caf71b94f410f7172b3
Alex Loren <lorenalexm@WR47H.local>
2023-06-19 06:31:20 -0700

    Created query request and tests

---
+++ Sources/StrapiSwiftCross/Requests/QueryRequest.swift
@@ -0,0 +1,19 @@
+//
+//  QueryRequest.swift
+//  
+//
+//  Created by Alex Loren on 6/10/23.
+//
+
+import Foundation
+
+public final class QueryRequest: StrapiRequest {
+    // MARK: - Functions
+    
+    /// Creates the query request for specific content.
+    /// - Parameters:
+    ///   - contentType: The Strapi type of this content.
+    public init(contentType: String) {
+        super.init(method: .get, contentType: contentType)
+    }
+}
---
+++ Tests/StrapiSwiftCrossTests/Requests/QueryRequestTests.swift
@@ -0,0 +1,29 @@
+//
+//  QueryRequestTests.swift
+//  
+//
+//  Created by Alex Loren on 6/19/23.
+//
+
+import XCTest
+@testable import StrapiSwiftCross
+
+class QueryRequestTests: XCTestCase {
+    /// Tests creating a new QueryRequest.
+    func testQueryRequest() {
+        let request = QueryRequest(contentType: "testing")
+        XCTAssertEqual(request.method, .get)
+        XCTAssertEqual(request.contentType, "testing")
+    }
+    
+    /// Tests creating a new QueryRequest with a filter.
+    func testQueryRequestWithFilters() {
+        let request = QueryRequest(contentType: "testing")
+        request.addFilter(type: .isNotNull, onField: "test", forValue: "testing")
+        
+        XCTAssertEqual(request.method, .get)
+        XCTAssertEqual(request.contentType, "testing")
+        XCTAssertEqual(request.filters!.count, 1)
+        XCTAssertEqual(request.filters![0].type, "$notNull")
+    }
+}
